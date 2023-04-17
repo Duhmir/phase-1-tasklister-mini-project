550601599ae8ba20b588368f8f6015832b61a5a6
+ <+>
2023-04-16 23:54:07 -0400

    yo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector('form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    handleToDo(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+
+function handleToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDeleteButton)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDeleteButton(e){
+  e.target.parentNode.remove()
+}
